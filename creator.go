36a6664de48ee213f54ff3ce2dae296e03d73ead
Rangel Reale <rangelspam@gmail.com>
2018-04-30 14:17:33 -0300

    Slice support

--- creator.go
+++ creator.go
@@ -13,16 +13,20 @@ type XCopyCreator interface {
 	SetField(index reflect.Value, value reflect.Value) error
 }
 
-func XCopyGetCreator(t reflect.Type) XCopyCreator {
-	switch reflectTypeIndirect(t).Kind() {
+func XCopyGetCreator(t reflect.Type) (XCopyCreator, error) {
+	switch rprim.IndirectType(t).Kind() {
 	case reflect.Struct:
-		return &XCopyCreator_Struct{t: t}
+		return &XCopyCreator_Struct{t: t}, nil
 	case reflect.Map:
-		return &XCopyCreator_Map{t: t}
-	case reflect.String, reflect.Int, reflect.Int32, reflect.Float32, reflect.Interface:
-		return &XCopyCreator_Primitive{t: t}
+		return &XCopyCreator_Map{t: t}, nil
+	case reflect.Slice:
+		return &XCopyCreator_Slice{t: t}, nil
+	case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32,
+		reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128, reflect.String, reflect.Interface:
+		return &XCopyCreator_Primitive{t: t}, nil
 	}
-	panic("Must handle all reflect kinds: " + t.Kind().String())
+	return nil, fmt.Errorf("Kind not supported: %s", t.Kind().String())
 }
 
 //
@@ -75,7 +79,7 @@ func (c *XCopyCreator_Struct) ensureValue() {
 		} else {
 			c.v = reflect.New(c.t).Elem()
 		}
-		c.it = reflectTypeIndirect(c.t)
+		c.it = rprim.IndirectType(c.t)
 	}
 }
 
@@ -129,7 +133,61 @@ func (c *XCopyCreator_Map) ensureValue() {
 
 func (c *XCopyCreator_Map) ensureValueOrZero() {
 	if !c.v.IsValid() {
-		c.v = reflect.New(c.t).Elem()
+		//c.v = reflect.New(c.t).Elem()
+		c.v = reflect.Zero(c.t)
+	}
+}
+
+//
+// Slice
+//
+
+type XCopyCreator_Slice struct {
+	t reflect.Type
+	v reflect.Value
+}
+
+func (c *XCopyCreator_Slice) Type() reflect.Type {
+	return c.t
+}
+
+func (c *XCopyCreator_Slice) Create() (reflect.Value, error) {
+	c.ensureValueOrZero()
+	return c.v, nil
+}
+
+func (c *XCopyCreator_Slice) SetField(index reflect.Value, value reflect.Value) error {
+	// convert index to int
+	sliceindex, err := rprim.Convert(index, reflect.TypeOf(0))
+	if err != nil {
+		return err
+	}
+
+	c.ensureValue()
+
+	// Add zero values until the index
+	for int(sliceindex.Int()) >= c.v.Len() {
+		c.v = reflect.Append(c.v, reflect.Zero(c.t.Elem()))
+	}
+
+	cv, err := XCopyToNew(value, c.t.Elem())
+	if err != nil {
+		return err
+	}
+
+	c.v.Index(int(sliceindex.Int())).Set(cv)
+	return nil
+}
+
+func (c *XCopyCreator_Slice) ensureValue() {
+	if !c.v.IsValid() {
+		c.v = reflect.MakeSlice(c.t, 0, 0)
+	}
+}
+
+func (c *XCopyCreator_Slice) ensureValueOrZero() {
+	if !c.v.IsValid() {
+		c.v = reflect.Zero(c.t)
 	}
 }
 
@@ -173,7 +231,7 @@ func (c *XCopyCreator_Primitive) ensureValue() {
 		} else {
 			c.v = reflect.New(c.t).Elem()
 		}
-		c.it = reflectTypeIndirect(c.t)
+		c.it = rprim.IndirectType(c.t)
 	}
 }
 
--- xcopy.go
+++ xcopy.go
@@ -1,27 +1,39 @@
 package goxcopy
 
 import (
-	"errors"
+	"fmt"
+	"github.com/RangelReale/rprim"
 	"reflect"
 )
 
 func XCopyToNew(src reflect.Value, destType reflect.Type) (reflect.Value, error) {
-	stype := reflectTypeIndirect(src.Type())
+	stype := rprim.IndirectType(src.Type())
 
 	switch stype.Kind() {
 	case reflect.Struct:
 		return XCopyToNew_Struct(src, destType)
-	case reflect.String, reflect.Int, reflect.Int16:
+	case reflect.Map:
+		return XCopyToNew_Map(src, destType)
+	case reflect.Slice:
+		return XCopyToNew_Slice(src, destType)
+	case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32,
+		reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128, reflect.String, reflect.Interface:
 		return XCopyToNew_Primitive(src, destType)
 	}
-
-	return reflect.Value{}, errors.New("Unknown source type: " + stype.Kind().String())
+	return reflect.Value{}, fmt.Errorf("Kind not supported: %s", stype.Kind().String())
 }
 
+//
+// Struct
+//
 func XCopyToNew_Struct(src reflect.Value, destType reflect.Type) (reflect.Value, error) {
 	srcValue := reflect.Indirect(src)
 
-	destCreator := XCopyGetCreator(destType)
+	destCreator, err := XCopyGetCreator(destType)
+	if err != nil {
+		return reflect.Value{}, err
+	}
 
 	for i := 0; i < srcValue.NumField(); i++ {
 		srcField := srcValue.Field(i)
@@ -34,27 +46,76 @@ func XCopyToNew_Struct(src reflect.Value, destType reflect.Type) (reflect.Value,
 
 		err := destCreator.SetField(reflect.ValueOf(srcFieldType.Name), srcField)
 		if err != nil {
-			return reflect.Value{}, err
+			return reflect.Value{}, fmt.Errorf("Error copying from struct field %s: %v", srcFieldType.Name, err)
 		}
 	}
 
 	return destCreator.Create()
 }
 
-func XCopyToNew_Primitive(src reflect.Value, destType reflect.Type) (reflect.Value, error) {
-	destCreator := XCopyGetCreator(destType)
+//
+// Map
+//
+func XCopyToNew_Map(src reflect.Value, destType reflect.Type) (reflect.Value, error) {
+	srcValue := reflect.Indirect(src)
 
-	err := destCreator.SetField(reflect.Value{}, src)
+	destCreator, err := XCopyGetCreator(destType)
 	if err != nil {
 		return reflect.Value{}, err
+	}
+
+	for _, k := range srcValue.MapKeys() {
+		srcField := srcValue.MapIndex(k)
+
+		err := destCreator.SetField(k, srcField)
+		if err != nil {
+			kstr, err := rprim.ConvertToString(k)
+			if err != nil {
+				kstr = "<unknown>"
+			}
+			return reflect.Value{}, fmt.Errorf("Error copying from map index %s: %v", kstr, err)
+		}
 	}
 
 	return destCreator.Create()
 }
 
-func reflectTypeIndirect(t reflect.Type) reflect.Type {
-	if t.Kind() == reflect.Ptr {
-		return t.Elem()
+//
+// Slice
+//
+func XCopyToNew_Slice(src reflect.Value, destType reflect.Type) (reflect.Value, error) {
+	srcValue := reflect.Indirect(src)
+
+	destCreator, err := XCopyGetCreator(destType)
+	if err != nil {
+		return reflect.Value{}, err
 	}
-	return t
+
+	for i := 0; i < srcValue.Len(); i++ {
+		srcField := srcValue.Index(i)
+
+		err := destCreator.SetField(reflect.ValueOf(i), srcField)
+		if err != nil {
+			return reflect.Value{}, fmt.Errorf("Error copying from slice index %d: %v", i, err)
+		}
+	}
+
+	return destCreator.Create()
+}
+
+//
+// Primitive
+//
+func XCopyToNew_Primitive(src reflect.Value, destType reflect.Type) (reflect.Value, error) {
+	destCreator, err := XCopyGetCreator(destType)
+	if err != nil {
+		return reflect.Value{}, err
+	}
+
+	err = destCreator.SetField(reflect.Value{}, src)
+	if err != nil {
+		return reflect.Value{}, err
+	}
+
+	return destCreator.Create()
 }
