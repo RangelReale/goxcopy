21b743d77374e593589d89c1091c36b3f1339dde
Rangel Reale <rangelspam@gmail.com>
2018-05-04 11:58:14 -0300

    Debug callback layout

--- intf.go
+++ intf.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"reflect"
+	"strings"
 )
 
 type Callback interface {
@@ -22,7 +23,8 @@ type Callback interface {
 // Debug callback
 
 type DebugCallback struct {
-	W io.Writer
+	W     io.Writer
+	level int
 }
 
 func NewDebugCallback(w io.Writer) *DebugCallback {
@@ -32,24 +34,33 @@ func NewDebugCallback(w io.Writer) *DebugCallback {
 }
 
 func (c *DebugCallback) BeginNew(ctx *Context, src reflect.Value, destType reflect.Type) {
-	fmt.Fprintf(c.W, ">>> BEGIN NEW: %s -> [%s]\n", destType.Kind(), ctx.FieldsAsString())
+	ts := strings.Repeat("\t", c.level+1)
+	c.level++
+	fmt.Fprintf(c.W, "%s>>> BEGIN NEW: %s -> [%s]\n", ts, destType.Kind(), ctx.FieldsAsString())
 }
 
 func (c *DebugCallback) EndNew(ctx *Context, src reflect.Value, destType reflect.Type) {
-	fmt.Fprintf(c.W, "<<< END NEW: %s -> [%s]\n", destType.Kind(), ctx.FieldsAsString())
+	c.level--
+	ts := strings.Repeat("\t", c.level+1)
+	fmt.Fprintf(c.W, "%s<<< END NEW: %s -> [%s]\n", ts, destType.Kind(), ctx.FieldsAsString())
 }
 
 func (c *DebugCallback) PushField(ctx *Context, fieldname reflect.Value, src reflect.Value, dest Creator) {
-	fmt.Fprintf(c.W, "+++ PUSH FIELD: %s -> [%s]\n", FieldnameToString(fieldname), ctx.FieldsAsString())
+	ts := strings.Repeat("\t", c.level+1)
+	c.level++
+	fmt.Fprintf(c.W, "%s+++ PUSH FIELD: %s -> [%s]\n", ts, FieldnameToString(fieldname), ctx.FieldsAsString())
 }
 
 func (c *DebugCallback) PopField(ctx *Context, fieldname reflect.Value, src reflect.Value, dest Creator) {
-	fmt.Fprintf(c.W, "--- POP FIELD: %s -> [%s]\n", FieldnameToString(fieldname), ctx.FieldsAsString())
+	c.level--
+	ts := strings.Repeat("\t", c.level+1)
+	fmt.Fprintf(c.W, "%s--- POP FIELD: %s -> [%s]\n", ts, FieldnameToString(fieldname), ctx.FieldsAsString())
 }
 
 func (c *DebugCallback) BeforeSetValue(ctx *Context, src reflect.Value, dest Creator, value reflect.Value) {
 }
 
 func (c *DebugCallback) AfterSetValue(ctx *Context, src reflect.Value, dest Creator, value reflect.Value) {
-	fmt.Fprintf(c.W, "=== SET VALUE: %s [%s]\n", ctx.FieldsAsString(), dest.Type().Kind().String())
+	ts := strings.Repeat("\t", c.level+1)
+	fmt.Fprintf(c.W, "%s=== SET VALUE: %s [%s]\n", ts, ctx.FieldsAsString(), dest.Type().Kind().String())
 }
